e3f3195823cce322d05d5a76643c51fe32b8fa62
Ksenia Hrisanova <kseniahrisanova07@gmail.com>
2025-10-25 15:27:37 +0300

    Добавлены файлы hello.asm и lab4.asm для лабораторной №4

---
+++ labs/lab04/lab4.asm
@@ -0,0 +1,17 @@
+SECTION .data                 ; Начало секции данных
+    hello: DB 'Хрисанова Ксения!',10 ; 'Хрисанова Ксения!' плюс символ перевода строки
+    helloLen: EQU $-hello       ; Длина строки hello
+
+SECTION .text                 ; Начало секции кода
+    GLOBAL _start
+
+_start:
+    mov eax,4                 ; Системный вызов для записи (sys_write)
+    mov ebx,1                 ; Описатель файла 1 = stdout
+    mov ecx,hello             ; Адрес строки hello
+    mov edx,helloLen          ; Длина строки
+    int 80h                   ; Вызов ядра
+
+    mov eax,1                 ; Системный вызов выхода (sys_exit)
+    xor ebx,ebx               ; Код возврата 0
+    int 80h                   ; Вызов ядра
